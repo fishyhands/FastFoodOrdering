39c9937a80386e105630345c0272c031e735d4fd
fishyhands <139359365+fishyhands@users.noreply.github.com>
2024-04-21 17:08:52 +0800

    added initialze payment when txt file is empty

--- src/Payment/PaymentMainMenu.java
+++ src/Payment/PaymentMainMenu.java
@@ -1,6 +1,16 @@
 package Payment;
 
+import java.util.ArrayList;
+
 public class PaymentMainMenu {
-    private static initialize
+    private static ArrayList<PaymentMethod> initializePayment(ArrayList<PaymentMethod> paymentMethods){
+        if (paymentMethods.isEmpty()){
+            paymentMethods.add(new CreditCard());
+            paymentMethods.add(new DebitCard());
+            paymentMethods.add(new OnlinePaymentPlatform());
+        }
+        return paymentMethods;
+    }
+
 
 }
