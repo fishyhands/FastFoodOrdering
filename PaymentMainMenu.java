97e0a41ef5e230e7aefa189cc6ac9c948b657a39
elbertyen <167335246+elbertyen@users.noreply.github.com>
2024-04-22 16:20:52 +0800

    added payment

---
+++ PaymentMainMenu.java
@@ -0,0 +1,23 @@
+package Payment;
+
+import java.util.ArrayList;
+import static Menu.MenuBrowsing.order;
+
+
+public class PaymentMainMenu {
+    private static ArrayList<PaymentMethod> initializePayment(ArrayList<PaymentMethod> paymentMethods){
+        if (paymentMethods.isEmpty()){
+            paymentMethods.add(new CreditCard());
+            paymentMethods.add(new DebitCard());
+            paymentMethods.add(new OnlinePaymentPlatform());
+        }
+        return paymentMethods;
+    }
+
+    public static void PaymentMenu(ArrayList<PaymentMethod> paymentMethods){
+        initializePayment(paymentMethods);
+        Payment.pay(paymentMethods);
+        order.setPaid(true);
+    }  
+}
+
