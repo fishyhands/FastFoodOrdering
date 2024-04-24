9cac08ad301fb685ea82040ffb2cbba62377db95
LongMingWei <longmw2001@gmail.com>
2024-04-24 14:59:56 +0800

    Update ChoiceMenu.java

--- src/Menu/ChoiceMenu.java
+++ src/Menu/ChoiceMenu.java
@@ -10,25 +10,32 @@ public class ChoiceMenu {
 
     public static void chooseItems(ArrayList<Menu> menuCategory) {
         int size = menuCategory.size();
-        Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < size; i++) {
-            System.out.println((i + 1) + ". " + menuCategory.get(i).getName() + "\t" + menuCategory.get(i).getPrice());
-        }
-        System.out.println("Enter your choice: ");
-        int choice = sc.nextInt();
-        itemName = menuCategory.get(choice-1).getName();
+        Scanner sc = new Scanner(System.in);  
+        boolean valid = false;
+        
+        do {       	
+        	for (int i = 0; i < size; i++) {
+                System.out.println((i + 1) + ". " + menuCategory.get(i).getName() + "\t" + menuCategory.get(i).getPrice());
+            }
+        	System.out.println("Enter your choice: ");
+        	int choice = sc.nextInt();
+        	if (choice <= 0 || choice > menuCategory.size()) {
+        		System.out.println("Invalid choice, please try again");
+        	}
+        	else {
+        		valid = true;
+        		itemName = menuCategory.get(choice-1).getName();
+        	}
+        } while (!valid);        
+        
         System.out.println("Enter the quantity: ");
         quantity = sc.nextInt();
-
         if (order.getCart().containsKey(itemName)){
-            int addQuantity = order.getCart().get(itemName) + quantity;
-            order.getCart().put(itemName, addQuantity);
-            System.out.println("Added to cart");
-        } else {
-            order.getCart().put(itemName, quantity);
-            System.out.println("Added to cart");
-        }
+            quantity = order.getCart().get(itemName) + quantity;
+        } 
+        
+        order.getCart().put(itemName, quantity);
+        System.out.println("Added to cart");        
     }
 
 }
