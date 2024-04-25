bad38a3d88105b49f6f7caf9d87654d6425417f5
elbertyen <167335246+elbertyen@users.noreply.github.com>
2024-04-25 22:19:11 +0800

    comment

--- src/exceptions/UnknownStaffRoleException.java
+++ src/exceptions/UnknownStaffRoleException.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+//a new exception for if staff role is not found in system. 
 public class UnknownStaffRoleException extends Exception {
     public UnknownStaffRoleException(String message) {
         super(message);
